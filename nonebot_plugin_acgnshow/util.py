96514cde47acf84cb20c85aafcf09a304ad46f73
Asankilp <asankilp@outlook.com>
2024-07-13 13:47:38 +0000

    🐛v0.1.4.1，随机背景图函数返回uri，修复Windows无背景图

--- nonebot_plugin_acgnshow/util.py
+++ nonebot_plugin_acgnshow/util.py
@@ -2,4 +2,5 @@ from .config import BGIMAGE_PATH
 import random
 def choose_random_bgimage():
     randomfile = random.choice(list(BGIMAGE_PATH.iterdir()))
-    return str(randomfile)
+    randomurl = randomfile.as_uri()
+    return randomurl
