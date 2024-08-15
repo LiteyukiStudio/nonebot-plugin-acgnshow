e82edfcae6dbc3ab2043a292dd1efb1a6d4061fb
snowy <snowykami@outlook.com>
2024-08-15 18:22:29 +0800

    :zap: 在请求中使用异步代替同步防止进程阻塞，优化部分代码样式

--- nonebot_plugin_acgnshow/util.py
+++ nonebot_plugin_acgnshow/util.py
@@ -1,10 +1,14 @@
 from .config import BGIMAGE_PATH
 import random
 import datetime
+
+
 def choose_random_bgimage():
     randomfile = random.choice(list(BGIMAGE_PATH.iterdir()))
     randomurl = randomfile.as_uri()
     return randomurl
+
+
 def convert_timestamp(timestamp):
     obj = datetime.datetime.fromtimestamp(timestamp)
     formatted_time = obj.strftime('%Y-%m-%d %H:%M:%S')
