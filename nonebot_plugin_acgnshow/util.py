50ad57ec222b49397a98161b6742ebbd760d4b71
EillesWan <W-YI_DoctorYI@outlook.com>
2024-08-22 22:50:18 +0800

    格式化代码，细节优化，修复了图片边框不完整的问题

--- nonebot_plugin_acgnshow/util.py
+++ nonebot_plugin_acgnshow/util.py
@@ -1,15 +1,21 @@
-from .config import BGIMAGE_PATH
+import os
 import random
 import datetime
 
+from .config import BGIMAGE_PATH
 
-def choose_random_bgimage():
-    randomfile = random.choice(list(BGIMAGE_PATH.iterdir()))
-    randomurl = randomfile.as_uri()
+
+def choose_random_bgimage() -> str:
+    """
+    从背景图片文件夹中随机选择一张图片，返回图片的uri地址
+    """
+    randomfile = random.choice(os.listdir(BGIMAGE_PATH))
+    randomurl = (BGIMAGE_PATH / randomfile).as_uri()
     return randomurl
 
 
-def convert_timestamp(timestamp):
-    obj = datetime.datetime.fromtimestamp(timestamp)
-    formatted_time = obj.strftime('%Y-%m-%d %H:%M:%S')
-    return formatted_time
+def convert_timestamp(timestamp) -> str:
+    """
+    将时间戳转换为日期格式
+    """
+    return datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
